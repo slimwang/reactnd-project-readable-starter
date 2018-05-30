929ade7a625294d2eb2447a433a492e5754926ff
Slim Wang <netbot@vip.qq.com>
2018-05-30 12:16:05 +0800

    add order bar

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Button, ButtonGroup } from 'reactstrap';
+import { MdAdd, MdSort } from 'react-icons/lib/md';
 import Post from './Post';
 
 class PostList extends Component {
   render() {
     return (
-      <Container>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </Container>
+      <div>
+        <Container className="mb-3 d-flex w-100 justify-content-between">
+          <ButtonGroup>
+            <Button outline color="primary">
+              <span className="mr-2"><MdSort /></span>Order by VoteScore
+            </Button>
+            <Button outline color="primary">
+              <span className="mr-2"><MdSort /></span>Order by Timestamp
+            </Button>
+          </ButtonGroup>
+          <Button outline color="primary">
+            <span className="mr-2"><MdAdd /></span>Add New Post
+          </Button>
+        </Container>
+        <Container>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </Container>
+      </div>
     );
   }
 }
