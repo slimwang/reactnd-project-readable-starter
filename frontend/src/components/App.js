fc0de9db13d15b7337e892ca5c73bdc93e4adfaf
Slim Wang <netbot@vip.qq.com>
2018-06-07 12:47:47 +0800

    add api

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,14 @@ const categories = ['All', 'Comedy', 'Science Fiction', 'Thriller'];
 
 class App extends Component {
   render() {
-    // API.getCategories().then(c => console.log(c));
+    const post = {
+      id: '6b33fce8-1745-f8de-4ad8-4ee42585oprf',
+      timestamp: Date.now(),
+      title: 'title',
+      body: 'body',
+      author: 'author',
+      category: 'react',
+    };
     return (
       <Container>
         <CategoryList categories={categories} />
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,120 @@ const api = 'http://localhost:3001';
 
 const headers = { Authorization: 'whatever-you-want' };
 
-export const getCategories = () =>
+const getCategories = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
     .then(data => data.categories);
+
+const getPosts = category =>
+  fetch(`${api}/${category}/posts`, { headers })
+    .then(res => res.json());
+
+const getAllPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json());
+
+const addPost = post =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+
+const getPostDetail = post =>
+  fetch(`${api}/posts/${post.id}`, { headers })
+    .then(res => res.json());
+
+const votePost = (post, type) =>
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ type }),
+  });
+
+const editPost = post =>
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title: post.title, body: post.body }),
+  });
+
+const deletePost = post =>
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+    },
+  });
+
+const getAllComments = post =>
+  fetch(`${api}/posts/${post.id}/comments`, { headers })
+    .then(res => res.json());
+
+const addComment = (post, comment) =>
+  fetch(`${api}/comments`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment),
+  });
+
+const getCommentDetail = comment =>
+  fetch(`${api}/comments/${comment.id}/`, { headers })
+    .then(res => res.json());
+
+const voteComment = (comment, type) =>
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ type }),
+  });
+
+const editComment = comment =>
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ timestamp: Date.now(), body: comment.body }),
+  });
+
+const deleteComment = comment =>
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+    },
+  });
+
+export {
+  getCategories,
+  getPosts,
+  getAllPosts,
+  addPost,
+  getPostDetail,
+  votePost,
+  editPost,
+  deletePost,
+  getAllComments,
+  addComment,
+  getCommentDetail,
+  voteComment,
+  editComment,
+  deleteComment,
+};
